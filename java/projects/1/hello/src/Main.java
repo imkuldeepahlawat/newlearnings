c778a34396cbae552b6c9f9f06a4a688e3a4fb97
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2023-01-16 12:43:12 +0530

    java complete with Hello world,variables,Object oriented Java,Conditionals and control flows,Arrays and arraylist,loops, String methods

--- java/projects/1/hello/src/Main.java
+++ java/projects/1/hello/src/Main.java
@@ -1,15 +1,40 @@
 public class Main {
     public static void main(String[] args) {
         //hello world printing
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
 //        variables declaring and printing
-        int a = 34;
-        float  b = 45.6f;
-        String c = "Kuldeep Singh";
-        char d = 'K';
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
+        int a = 348;
+//        float  b = 45.6f;
+//        String c = "Kuldeep Singh";
+//        char d = 'K';
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+//        java condition statements
+//        System.out.println("Condition Statements");
+//        if(a >= 50){
+//            System.out.println("First if Statements is complete");
+//        } else if (a == 34) {
+//            System.out.println("else if statement is complete");
+//        } else{
+//            System.out.println("Default Statement");
+//        }
+//        System.out.println("switch Case Testing");
+//        switch (a){
+//            case 34:
+//                System.out.println("a is 34");
+//                break;
+//            case 35:
+//                System.out.println("a is 35");
+//                break;
+//            case 36:
+//                System.out.println("a is 36");
+//                break;
+//            default:
+//                System.out.println("Default Case Running");
+//        }
+//        loop testing
+        
     }
 }
