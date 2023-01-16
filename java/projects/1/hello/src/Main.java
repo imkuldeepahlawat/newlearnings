3abd7f8dff08a6086e260640ed59eacd5618e51e
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2023-01-16 11:45:59 +0530

    First Commit adding jabvascript cheatesheet and java code

---
+++ java/projects/1/hello/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //hello world printing
+        System.out.println("Hello world!");
+//        variables declaring and printing
+        int a = 34;
+        float  b = 45.6f;
+        String c = "Kuldeep Singh";
+        char d = 'K';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
