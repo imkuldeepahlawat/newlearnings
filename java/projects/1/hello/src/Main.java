d62df3baff70f4e7f1c8139706ce961aecab64f7
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2023-01-20 10:44:41 +0530

    prectice js code

--- java/projects/1/hello/src/Main.java
+++ java/projects/1/hello/src/Main.java
@@ -1,9 +1,12 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //hello world printing
 //        System.out.println("Hello world!");
 //        variables declaring and printing
-        int a = 348;
+//        int a = 348;
 //        float  b = 45.6f;
 //        String c = "Kuldeep Singh";
 //        char d = 'K';
@@ -35,6 +38,45 @@ public class Main {
 //                System.out.println("Default Case Running");
 //        }
 //        loop testing
-        
+//        syntaxes
+//        while(true){
+//            System.out.println(("Hello This is while loop"));
+//            break;
+//        }
+////        for loop
+//        System.out.println("for loop using print");
+//        for (int i = 0;i<10;i++){
+//            System.out.print(i);
+//        }
+//        System.out.println("\nfor loop using println");
+//        for (int i = 0;i<10;i++){
+//            System.out.println(i);
+//        }
+//        making function for average and print stars
+//        System.out.println(avg(5,7));
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter Your roll Number");
+//        int a = input.nextInt();
+//        float b = input.nextFloat();
+//        System.out.println(a);
+//        System.out.println(b);
+//        making array
+//        int a[] = new int[5];
+//        int d[] = {1,2,3,4,5};
+//        String a = "my name is kuldeep";
+//        System.out.println(a.length());
+//        a[0] = 5;
+//        System.out.println(a[0]);
+//        System.out.println(a[2]);
+//        making 2d arrays
+        int b[][] = new int[2][2];
+        int c[][] ={{1,2}
+                ,{2,4}};
+
+    }
+//    average function
+    static int avg(int a,int b){
+        int sum = a + b;
+        return sum/2;
     }
 }
---
+++ javascript/code/1.js
@@ -0,0 +1,7 @@
+console.log("Namaste Duniya");
+let a = 10;
+var b = 9;
+const c = "this is a js file";
+for(let i = 0;i<a;i++){
+    console.log("This is loop "+i);
+}
